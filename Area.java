3fc57e6667c7871c63185819ab87267a26db08b4
Jaywardhan Shinde <102287791+Jayshinde09@users.noreply.github.com>
2023-06-19 22:01:46 +0530

    Rectangle.java

--- Area.java
+++ Area.java
@@ -1,14 +1,23 @@
+
 import java.util.Scanner;
-public class Area
-{
- public static void main(String[] args) 
- {
-  Scanner sc = new Scanner(System.in);
 
-  System.out.println(" radius of the circle is: ");
-  double radius = sc.nextDouble();
+public class Rectangle {
 
-  System.out.println("Perimeter is = " + (2 * radius * Math.PI));
-  System.out.println("Area is = " + (Math.PI * radius * radius));
- }
+    public static void main(String[] args) {
+
+        float length, width, area, perimeter;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Height of rectangle: ");
+        length = in.nextFloat();
+
+        System.out.print("Enter width of rectangle: ");
+        width = in.nextFloat();
+
+        perimeter = 2 * (length + width);
+        area = length * width;
+
+        System.out.println("Perimeter of rectangle is " + perimeter + " units.");
+        System.out.println("Area of rectangle is " + area + " sq. units.");
+    }
 }
